57f179c91481662d3cf90cd69e464f3999e2dbb9
Meena <meena.lalchand17@gmail.com>
2026-03-23 20:44:32 +0530

    Logout + Session destroy + Navbar update

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,21 +1,23 @@
 <div class="navbar">
 
     <a href="/alumni_v2/index.php">Home</a>
-    <a href="#">Alumni</a>
+    <a href="/alumni_v2/modules/alumni/alumni_list.php">Alumni</a>
     <a href="#">Forum</a>
     <a href="#">Jobs</a>
     <a href="#">Events</a>
     <a href="#">Gallery</a>
     <a href="#">Login</a>
 
-<?php if (isset($_SESSION['user_name'])) { ?>
+    <?php if (isset($_SESSION['user_name'])) { ?>
 
-    Welcome, <?php echo $_SESSION['user_name']; ?>
+        Welcome, <?php echo $_SESSION['user_name']; ?>
 
-<?php } else { ?>
+        <a href="/alumni_v2/modules/auth/logout.php">Logout</a>
 
-    <a href="/alumni_v2/modules/auth/login.php">Login</a>
+    <?php } else { ?>
 
-<?php } ?>
+        <a href="/alumni_v2/modules/auth/login.php">Login</a>
+
+    <?php } ?>
 
 </div>
---
+++ modules/alumni/alumni_list.php
@@ -0,0 +1,3 @@
+<?php include "../../includes/header.php"; ?>
+<?php include "../../includes/auth_check.php"; ?>
+<h2> Hi Alumni! </h2>
