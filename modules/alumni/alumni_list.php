92c8c1ee4760e8c8f45cfc3b591c96d029b86f64
Meena <meena.lalchand17@gmail.com>
2026-03-27 20:42:07 +0530

    Add alumni_list page with DB fetch and table display

--- modules/alumni/alumni_list.php
+++ modules/alumni/alumni_list.php
@@ -1,3 +1,28 @@
 <?php include "../../includes/header.php"; ?>
 <?php include "../../includes/auth_check.php"; ?>
+<?php include "../../config/database.php"; ?>
 <h2> Hi Alumni! </h2>
+
+<?php $query = "SELECT * FROM alumni ORDER BY id DESC";
+$result = mysqli_query($conn, $query);
+?>
+<table border="1" cellpadding="10">
+    <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Course</th>
+        <th>Batch</th>
+        <th>Job</th>
+    </tr>
+    <?php while ($row = mysqli_fetch_assoc($result)){ ?>
+<tr>
+    <td><?php echo $row['name']; ?></td>
+    <td><?php echo $row['email']; ?></td>
+    <td><?php echo $row['course']; ?></td>
+    <td><?php echo $row['batch']; ?></td>
+    <td><?pho echo $row['job']; ?></td>
+</tr>
+<?php } ?>
+
+</table>
+<?php include "../../includes/footer.php"; ?>
