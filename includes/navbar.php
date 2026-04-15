f8e6ea827e260cfdb65233921f9ac83d9c818304
Meena <meena.lalchand17@gmail.com>
2026-04-15 20:16:54 +0530

    Fix navbar database connection for avatar query

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,3 +1,6 @@
+<?php
+include_once __DIR__ . "/../config/database.php";
+?>
 <div class="navbar">
 
     <a href="/alumni_v2/index.php">Home</a>
@@ -7,19 +10,27 @@
     <a href="#">Events</a>
     <a href="#">Gallery</a>
 
-    <?php if (isset($_SESSION['user_name'])) { ?>
+ <?php if (isset($_SESSION['user_id'])) { ?>
 
-        <a href="/alumni_v2/modules/alumni/my_profile.php">My Profile</a>
+    <?php
+    $user_id = $_SESSION['user_id'];
+    $query = "SELECT profile_image, name FROM alumni WHERE user_id='$user_id'";
+    $result = mysqli_query($conn, $query);
+    $user = mysqli_fetch_assoc($result);
+    ?>
 
-        <span>Welcome, <?php echo $_SESSION['user_name']; ?></span>
+    <a href="/alumni_v2/modules/alumni/my_profile.php">
+        <img src="/alumni_v2/assets/uploads/<?php echo $user['profile_image'] ?: 'default.png'; ?>" width="30" style="border-radius:50%; vertical-align:middle;">
+        <?php echo $user['name']; ?>
+    </a>
 
-        <a href="/alumni_v2/modules/auth/logout.php">Logout</a>
+    <a href="/alumni_v2/modules/auth/logout.php">Logout</a>
 
-    <?php } else { ?>
+  
 
-        <a href="/alumni_v2/modules/auth/login.php">Login</a>
-        <a href="/alumni_v2/modules/auth/signup.php">Signup</a>
-
-    <?php } ?>
+<?php } else { ?>
+    <a href="/alumni_v2/modules/auth/login.php">Login</a>
+    <a href="/alumni_v2/modules/auth/signup.php">Signup</a>
+<?php } ?>
 
 </div>
