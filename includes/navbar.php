3d47831af3c11ea206e0e982d9401265db4f93e6
Meena <meena.lalchand17@gmail.com>
2026-04-24 20:20:22 +0530

    Implement smart navbar with active link highlighting

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,17 +1,23 @@
 <?php
 include_once __DIR__ . "/../config/database.php";
+$current_page = basename($_SERVER['PHP_SELF']);
 ?>
+
 <div class="navbar">
 
-    <a href="/alumni_v2/index.php">Home</a>
-    <a href="/alumni_v2/modules/alumni/alumni_list.php">Alumni</a>
+    <a class="<?php echo ($current_page == 'index.php') ? 'active' : ''; ?>"
+        href="/alumni_v2/index.php">Home</a>
+
+    <a class="<?php echo ($current_page == 'alumni_list.php') ? 'active' : ''; ?>"
+        href="/alumni_v2/modules/alumni/alumni_list.php">Alumni</a>
     <a href="#">Forum</a>
     <a href="#">Jobs</a>
     <a href="#">Events</a>
     <a href="#">Gallery</a>
 
     <?php if (isset($_SESSION['role']) && $_SESSION['role'] === 'admin') { ?>
-        <a href="/alumni_v2/modules/admin/dashboard.php">Dashboard</a>
+        <a class="<?php echo ($current_page == 'dashboard.php') ? 'active' : ''; ?>"
+            href="/alumni_v2/modules/admin/dashboard.php">Dashboard</a>
     <?php } ?>
 
     <?php if (isset($_SESSION['user_id'])) { ?>
@@ -23,14 +29,13 @@ include_once __DIR__ . "/../config/database.php";
         $user = mysqli_fetch_assoc($result);
         ?>
 
-        <a href="/alumni_v2/modules/alumni/my_profile.php">
+        <a class="<?php echo ($current_page == 'my_profile.php') ? 'active' : ''; ?>"
+            href="/alumni_v2/modules/alumni/my_profile.php">
             <img src="/alumni_v2/assets/uploads/<?php echo $user['profile_image'] ?: 'default.png'; ?>" width="30" style="border-radius:50%; vertical-align:middle;">
             <?php echo $user['name']; ?>
         </a>
 
         <a href="/alumni_v2/modules/auth/logout.php">Logout</a>
-
-
 
     <?php } else { ?>
         <a href="/alumni_v2/modules/auth/login.php">Login</a>
